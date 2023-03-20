2b30b4822de5494adaa1973e318f7263498fc7f7
henryjrtena <henrytena2013@gmail.com>
2023-03-20 18:02:50 +0800

    feat: remove default properties - remove default properties on Freezed Model

--- lib/api/model/pokemon.dart
+++ lib/api/model/pokemon.dart
@@ -6,8 +6,8 @@ part 'pokemon.g.dart';
 @freezed
 class Pokemon with _$Pokemon {
   const factory Pokemon({
-    @JsonKey(name: 'name') @Default('') String name,
-    @JsonKey(name: 'url') @Default('') String url,
+    @JsonKey(name: 'name') required String name,
+    @JsonKey(name: 'url') required String url,
   }) = _Pokemon;
 
   factory Pokemon.fromJson(Map<String, dynamic> json) =>
