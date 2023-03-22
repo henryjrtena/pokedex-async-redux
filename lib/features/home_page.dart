b312a97290fe7e3962ccc863105af959376460c5
henryjrtena <henrytena2013@gmail.com>
2023-03-22 18:10:15 +0800

    feat: implementation of getting the pokemon details - added the action for getting the pokemon details - added a state in the AppState for pokemon details - added a dispatch call for GetPokemonDetails in HomePage - resolved all dart linter and flutter analyzed

--- lib/features/home_page.dart
+++ lib/features/home_page.dart
@@ -1,6 +1,7 @@
+import 'package:pokedex_async_redux/state/action/pokemon_actions.dart';
+import 'package:pokedex_async_redux/state/action/pokemon_details_actions.dart';
 import 'package:async_redux/async_redux.dart';
 import 'package:flutter/material.dart';
-import 'package:pokedex_async_redux/state/action/pokemon_actions.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -8,6 +9,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     StoreProvider.dispatch(context, GetPokemonsAction());
+    StoreProvider.dispatch(context, GetPokemonDetailsAction());
 
     return MaterialApp(
       // TODO: To remove all hard-coded text and create constant.
