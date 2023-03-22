055ea8788c02ac385bba5ff34110b76aea21e19f
henryjrtena <henrytena2013@gmail.com>
2023-03-22 12:01:14 +0800

    feature: flutter analyzed and dart formatting - removed unused imports - dart formatted main, app_state, api_service and pokemon_api.dart

--- lib/api/handler/pokemon_api.dart
+++ lib/api/handler/pokemon_api.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:pokedex_async_redux/api/api_client.dart';
 import 'package:pokedex_async_redux/api/model/pokemon.dart';
 import 'package:pokedex_async_redux/api/model/pokemon_details.dart';
@@ -24,13 +22,10 @@ class PokemonApi {
     queryParameters['limit'] = limit;
     queryParameters['offset'] = offset;
 
-    final uri = baseUri.replace(
-        queryParameters: queryParameters, path: '${baseUri.path}/pokemon');
+    final uri = baseUri.replace(queryParameters: queryParameters, path: '${baseUri.path}/pokemon');
 
     return await apiClient.dio.getUri(uri).then((response) {
-      return response.data['results']
-          .map<Pokemon>((dynamic data) => Pokemon.fromJson(data as Json))
-          .toList();
+      return response.data['results'].map<Pokemon>((dynamic data) => Pokemon.fromJson(data as Json)).toList();
     });
   }
 
